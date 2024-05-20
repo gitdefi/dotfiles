f2b4efc3278a132b61cea14bfcf003184a8fc283
gitdefi <108360433+gitdefi@users.noreply.github.com>
2024-05-20 15:19:34 +0800

    Add files via upload

---
+++ Quil/quil_node-info.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# # # > 参考 
+# ip=$(curl -4 -s -X GET http://checkip.amazonaws.com --max-time 10)
+# > 
+# ip=$(curl -s http://ipv4.icanhazip.com)
+# # 备用
+# # ip=$(curl -s https://api.ipify.org)
+# > 
+# CURRENT_TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+# INTERFACE_IP=`ip address show eth0 | grep eth0 | grep global | awk '{print$2}'`
+# DNS_RECORD_IP=`dig ${DOMAIN} | grep "${DOMAIN}" | grep -v ';' | awk '{print$5}'`
+
+# echo -e "\n$CURRENT_TIMESTAMP $ip" >> /root/ddns_ip_list.txt
+
+
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+# output_dir="$(mktemp -d)"
+output_dir="/root"
+qnode_output=$(cd /root/ceremonyclient/node && qnode -node-info)
+
+# current_max_frame=$(echo "$output" | grep -oP 'Max Frame: \K\d+')
+max_frame=$(echo "$qnode_output" | grep -P 'Max Frame:')
+
+# echo "\n\n==> Quil Node Info \n$current_time: \n$output" >> "$output_dir/quil_node-info.log"
+
+# echo "\n\n==> $current_time \nQuil Node Info: \n$output" >> "$output_dir/quil_node-info.log"
+
+echo "\n\n$current_time \n$max_frame <== " >> "$output_dir/quil_node-info.log"
+
